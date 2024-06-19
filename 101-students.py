ba691c9834e74562d89102e16a23d33f5f329547
HudaOmer <hudaoe175@gmail.com>
2024-06-19 02:03:38 +0000

    Solve Advanced 100

---
+++ 101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+14. Top students
+"""
+
+
+def top_students(mongo_collection):
+    """
+    order students by score
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
